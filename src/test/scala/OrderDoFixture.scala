44b66d05116a601dd5588393e7b7e93f6066d102
Brian Hartin <brian.hartin@banno.com>
2015-04-30 12:02:43 -0500

    Added OrderDoFixture, fitnesse test fails with 404 because we haven't added route yet.  #Broken

---
+++ src/test/scala/OrderDoFixture.scala
@@ -0,0 +1,39 @@
+package slickexample
+
+import fitlibrary.DoFixture
+
+import net.sourceforge.jwebunit.junit.JWebUnit._
+
+
+class OrderDoFixture extends DoFixture {
+  setBaseUrl("http://localhost:8080")
+
+  def given = this
+
+  def then = this
+
+  def when = this
+
+  def aCleanDatabase = {
+    beginAt("/db/create-db")
+  }
+
+  def aUserVisits(path: String) = {
+    beginAt(path)
+  }
+
+  def theResponseCodeIs: Int = {
+    web.getServerResponseCode
+  }
+
+  def thePageTextIs: String = {
+    web.getPageText
+  }
+
+
+  // This is only needed to get around a FitNesse bug that
+  // will produce a harmless, but noisy, error message.
+  def slickexampleDotOrderDoFixture() = {}
+
+  private def web = getTestingEngine
+}
